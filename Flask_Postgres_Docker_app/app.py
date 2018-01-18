0a0f5fc3e7eaad0a844c27e99893abc8291ace11
sara <sara@vijays-MacBook-Pro.local>
2018-01-18 15:13:13 -0800

    modified the templates and models, could return the simple table

--- Flask_Postgres_Docker_app/app.py
+++ Flask_Postgres_Docker_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 from models import *
-from models import User
+from models import User, Movie, engine
 
 app = Flask(__name__)
 
@@ -9,23 +9,55 @@ app = Flask(__name__)
 def create_movie():
 	if request.method == 'POST':
 		# check if the user already exists
-		check_existing_user = session.query(User).filter(User.name==request.form['actor_name'])
-		if check_existing_user:
-			return redirect(url_for('display_warning'))
+		check_existing_user = session.query(User.id).filter(User.name==request.form['actor_name']).scalar()
+		check_movie_exists = session.query(Movie).filter(Movie.moviename == request.form['movie-name']).scalar()
+		if check_existing_user and check_movie_exists:
+			return "Movie and the role player already exists! Thanks for participation!"
+		elif check_existing_user:
+			# add new movie
+			add_newmovie = Movie(moviename=request.form['movie-name'], movie_releaseyear=request.form['release-year'], movie_imdb=request.form['imdb-link'], roleid=check_existing_user.id)
+			session.add(add_newmovie)
+			session.commit()
+			return redirect(url_for('hello'))
 		# else create a new row in the User table
 		else:
-			add_new_movie = User(name=request.form['actor_name'], moviename=request.form['movies'])
-			session.add(add_new_movie)
+			add_newuser = User(name=request.form['actor_name'])
+			session.add(add_newuser)
 			session.commit() 
+			add_newmovie = Movie(moviename=request.form['movie-name'], movie_releaseyear=request.form['release-year'], movie_imdb=request.form['imdb-link'], roleid=add_newuser.id)
+			session.add(add_newmovie)
+			session.commit()
 			return redirect(url_for('hello'))
 	# diaply the form to create a new movie row 
 	else:
 		return render_template('main.html')
 
-# already existing user in the database
-@app.route('/alreadyexists')
-def display_warning():
-	return "Actor/Actress is already existing in the database"
+@app.route('/search', methods=['GET', 'POST'])
+def search_page():
+	if request.method == 'POST':
+		if len(request.form['search_movie']) > 0:
+			return redirect(url_for('display_movie', searchmovie=request.form['search_movie']))
+		elif len(request.form['role_name']) > 0:
+			return redirect(url_for('display_role', searchrole=request.form['role_name']))
+	else:
+		return render_template('searchpage.html')
+
+@app.route('/displayrole/<searchrole>', methods=['POST', 'GET'])
+def display_role(searchrole):
+	print("Search rolesssss", searchrole)
+	get_role_name = session.query(User.id, User.name).filter(User.name==searchrole)
+	if get_role_name.scalar() is not None:
+		return render_template('display_roleplayer.html', display=get_role_name)
+	else:
+		return "role player doesn't exist in our database"
+
+@app.route('/displaymovie/<searchmovie>', methods=['POST', 'GET'])
+def display_movie(searchmovie):
+	get_movie = session.query(Movie).filter(Movie.moviename==searchmovie)
+	if get_movie.scalar() is not None:
+		return render_template('display_movie.html', display=get_movie)
+	else:
+		return "Movie doesn't exist in the database"
 
 @app.route('/')
 def hello():
--- Flask_Postgres_Docker_app/models.py
+++ Flask_Postgres_Docker_app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Integer, ForeignKey  
 from sqlalchemy.ext.declarative import declarative_base  
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 import os
 
 user = os.environ['POSTGRES_USER']
@@ -18,21 +18,57 @@ engine = create_engine('postgresql://{user1}:{passwd1}@{host1}:{port1}/{db1}'.fo
         port1=port,
         db1=db))
 
-# engine = create_engine('postgresql://saranya:mynewpassword@localhost/movie_db')
 # bind the postgres to the session 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 Base = declarative_base()
 
-# simple movie table
-class User(Base):
-	__tablename__ = 'movie_table'
+# # simple movie table
+# class User(Base):
+# 	__tablename__ = 'movie_table'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), unique=True, nullable=False)
+# 	id = Column(Integer, primary_key=True)
+# 	name = Column(String(250), unique=True, nullable=False)
+# 	moviename = Column(String(250), nullable=False)
+class User(Base):
+	__tablename__ = 'roleplayer'
+
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	name = Column(String(250), unique=True, nullable=True)
+	movie = relationship('Movie', backref='roleplayer')
+
+
+class Movie(Base):
+	__tablename__ = 'movie'
+
+	id = Column(Integer, primary_key=True, autoincrement=True)
 	moviename = Column(String(250), nullable=False)
+	movie_releaseyear = Column(Integer)
+	movie_imdb = Column(String(500), nullable=False)
+	roleid = Column(Integer, ForeignKey('roleplayer.id'))
+
 
 def __init__database():
 	# create all the database tables
 	Base.metadata.create_all(engine)
+
+
+# # engine = create_engine('postgresql://saranya:mynewpassword@localhost/movie_db')
+# # bind the postgres to the session 
+# Session = sessionmaker(bind=engine)
+# session = Session()
+
+# Base = declarative_base()
+
+# class User(Base):
+# 	__tablename__ = 'movie_table'
+
+# 	id = Column(Integer, primary_key=True)
+# 	name = Column(String(250), unique=True, nullable=False)
+# 	moviename = Column(String(250), nullable=False)
+
+
+# def __init__database():
+# 	# create all the database tables
+# 	Base.metadata.create_all(engine)
