a645d262153e6583ecdd3762eb5090ff45d91733
sara <sara@vijays-MacBook-Pro.local>
2018-01-29 11:26:59 -0800

    added json result format

--- Flask_Postgres_Docker_app/app.py
+++ Flask_Postgres_Docker_app/app.py
@@ -1,37 +1,45 @@
-from flask import Flask, request, render_template, redirect, url_for
-from models import *
-from models import User, Movie, engine
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from models import User, Movie, engine, session, create_engine
+import webbrowser
 
 app = Flask(__name__)
 
+engine = create_engine('postgresql://saranya:mynewpassword@localhost/movie_db')
+db_conn= engine.connect()
+
+
+# for the web
 # create a new row in the movie table 
 @app.route('/main/createnewmovie', methods=['GET', 'POST'])
 def create_movie():
 	if request.method == 'POST':
 		# check if the user already exists
 		check_existing_user = session.query(User.id).filter(User.name==request.form['actor_name']).scalar()
-		check_movie_exists = session.query(Movie).filter(Movie.moviename == request.form['movie-name']).scalar()
+		check_movie_exists = session.query(Movie).filter(Movie.movie_title == request.form['movie-name']).scalar()
 		if check_existing_user and check_movie_exists:
 			return "Movie and the role player already exists! Thanks for participation!"
 		elif check_existing_user:
 			# add new movie
-			add_newmovie = Movie(moviename=request.form['movie-name'], movie_releaseyear=request.form['release-year'], movie_imdb=request.form['imdb-link'], roleid=check_existing_user.id)
+			add_newmovie = Movie(movieimdbid=request.form['movie-imdb'],movie_title=request.form['movie-name'], movie_rating=request.form['ratings'],movie_releaseyear=request.form['release-year'],movie_votes=request.form['votes'])
 			session.add(add_newmovie)
 			session.commit()
-			return redirect(url_for('hello'))
+			return redirect(url_for('movieimdb_status'))
 		# else create a new row in the User table
 		else:
-			add_newuser = User(name=request.form['actor_name'])
-			session.add(add_newuser)
-			session.commit() 
-			add_newmovie = Movie(moviename=request.form['movie-name'], movie_releaseyear=request.form['release-year'], movie_imdb=request.form['imdb-link'], roleid=add_newuser.id)
+			add_newmovie = Movie(movieimdbid=request.form['movie-imdb'],movie_title=request.form['movie-name'], movie_rating=request.form['ratings'],movie_releaseyear=request.form['release-year'],movie_votes=request.form['votes'])
 			session.add(add_newmovie)
 			session.commit()
-			return redirect(url_for('hello'))
+			#find movie imdb id
+			imdb_movie_getid = session.query(Movie).filter(Movie.movies_acted==request.form['acted'])			
+			add_newuser = User(name=request.form['actor_name'], birthyear=request.form['birthday'],deathyear=request.form['deathday'], primaryprofession=request.form['proff'], movies_acted=request.form['acted'])
+			session.add(add_newuser)
+			session.commit() 
+			return redirect(url_for('movieimdb_status'))
 	# diaply the form to create a new movie row 
 	else:
 		return render_template('main.html')
 
+# search from the webbrowser end for either using movie name or role name
 @app.route('/search', methods=['GET', 'POST'])
 def search_page():
 	if request.method == 'POST':
@@ -44,23 +52,77 @@ def search_page():
 
 @app.route('/displayrole/<searchrole>', methods=['POST', 'GET'])
 def display_role(searchrole):
-	print("Search rolesssss", searchrole)
-	get_role_name = session.query(User.id, User.name).filter(User.name==searchrole)
-	if get_role_name.scalar() is not None:
+	get_role_name = session.execute("SELECT * FROM roleplayer WHERE name='%s';" % (searchrole))
+	get_role_namecount = session.execute("SELECT * FROM roleplayer WHERE name='%s';" % (searchrole)).scalar()
+	if get_role_namecount is not None:
 		return render_template('display_roleplayer.html', display=get_role_name)
 	else:
 		return "role player doesn't exist in our database"
 
 @app.route('/displaymovie/<searchmovie>', methods=['POST', 'GET'])
 def display_movie(searchmovie):
-	get_movie = session.query(Movie).filter(Movie.moviename==searchmovie)
+	get_movie = session.query(Movie).filter(Movie.movie_title==searchmovie)
 	if get_movie.scalar() is not None:
 		return render_template('display_movie.html', display=get_movie)
 	else:
 		return "Movie doesn't exist in the database"
 
-@app.route('/')
-def hello():
+# for api search
+@app.route('/apisearch', methods=['GET', 'POST'])
+def search():
+	# check the param if the search is based on movie or roleplayer
+	if request.method == 'GET':
+		if request.args.get('search_movie') is not None:
+			return displaymovie_api(request.args.get('search_movie'))
+		if request.args.get('search_role') is not None:
+			return displayrole_api(request.args.get('search_role'))
+
+	
+@app.route('/movie/roleplayer', methods=['POST', 'GET'])
+def displayrole_api(searchrole):
+	get_role_name = session.execute("SELECT * FROM roleplayer WHERE name='%s';" % (searchrole))	
+	get_role_name_count = session.execute("SELECT * FROM roleplayer WHERE name='%s';" % (searchrole)).scalar()
+
+	if get_role_name_count > 0:                                                         #if the count is greater than 0, then result exists and extract the movie based on the role player acted columnresult
+		json_string = {"Role Player":[], "Movies acted":[]}
+        
+        for i in get_role_name:
+        	json_string["Role Player"].append([{"Name": i.name, "Birthyear":i.birthyear, "primaryprofession":i.primaryprofession}])
+        
+        get_movies_acted = session.execute("SELECT movies_acted FROM roleplayer WHERE name='%s';" % (searchrole))
+
+        for k in get_movies_acted:
+	    	res = k[0].split(',')
+	    	for i in res:
+	    		acted_movies_count = session.execute("SELECT * FROM movie1 WHERE movieimdbid='%s';" % (i)).scalar()
+	    		if acted_movies_count > 0:
+	    			acted_movies = session.execute("SELECT * FROM movie1 WHERE movieimdbid='%s';" % (i.encode("utf-8")))
+	    			for cols in acted_movies:
+	    				json_string["Movies acted"].append([{'Title':cols.movie_title, 'imdb_id':cols.movieimdbid, 'Released_on':cols.movie_releaseyear, 'Ratings':cols.movie_rating,'votes':cols.movie_votes}])
+			
+		return jsonify(json_string)
+	else:
+		return "role player doesn't exist in our database"
+
+
+@app.route('/movie/moviedescription', methods=['POST', 'GET'])
+def displaymovie_api(searchmovie):
+	get_movie = session.execute("SELECT * FROM movie1 WHERE movie_title='%s';" % (searchmovie))
+	get_no_of_movies = session.execute("SELECT * FROM movie1 WHERE movie_title='%s';" % (searchmovie)).scalar()
+	json_string = {"Movie details": []}
+	if get_no_of_movies > 0:
+		for i in get_movie:
+			json_string['Movie details'].append([{'Title':i.movie_title, 'imdb_id':i.movieimdbid, 'Released_on':i.movie_releaseyear, 'Ratings':i.movie_rating,'votes':i.movie_votes}])
+		return jsonify(json_string)
+	else:
+		return "Movie doesn't exist in the database"
+
+@app.route('/imdbredirect/<movieimdb_id>')
+def imdbpage(movieimdb_id):
+	return redirect(webbrowser.open("http://www.imdb.com/title/%s"%(movieimdb_id)))
+
+@app.route('/movieimdb_status')
+def movieimdb_status():
 	return "hello successfully created the new movie!"
 
 if __name__ == "__main__":
--- Flask_Postgres_Docker_app/models.py
+++ Flask_Postgres_Docker_app/models.py
@@ -1,25 +1,31 @@
-from sqlalchemy import Column, String, Integer, ForeignKey  
+from sqlalchemy import Column, String, Integer, ForeignKey 
 from sqlalchemy.ext.declarative import declarative_base  
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
-import os
+from sqlalchemy.orm import sessionmaker, relationship, scoped_session
+from sqlalchemy.types import ARRAY, Float
+import psycopg2
 
-user = os.environ['POSTGRES_USER']
-passwd = os.environ['POSTGRES_PASSWORD']
-db = os.environ['POSTGRES_DB']
-host = 'db'
-port='5432'
+
+# import os
+
+# user = os.environ['POSTGRES_USER']
+# passwd = os.environ['POSTGRES_PASSWORD']
+# db = os.environ['POSTGRES_DB']
+# host = 'db'
+# port='5432'
 
 # get the postgresql database url
-engine = create_engine('postgresql://{user1}:{passwd1}@{host1}:{port1}/{db1}'.format(
-        user1=user,
-        passwd1=passwd,
-        host1=host,
-        port1=port,
-        db1=db))
+# engine = create_engine('postgresql://{user1}:{passwd1}@{host1}:{port1}/{db1}'.format(
+#         user1=user,
+#         passwd1=passwd,
+#         host1=host,
+#         port1=port,
+#         db1=db))
 
+engine = create_engine('postgresql://saranya:mynewpassword@localhost/movie_db')
 # bind the postgres to the session 
 Session = sessionmaker(bind=engine)
+multi_thrdconn = scoped_session(Session)
 session = Session()
 
 Base = declarative_base()
@@ -31,44 +37,69 @@ Base = declarative_base()
 # 	id = Column(Integer, primary_key=True)
 # 	name = Column(String(250), unique=True, nullable=False)
 # 	moviename = Column(String(250), nullable=False)
+
+class Movie(Base):
+	__tablename__ = 'movie1'
+
+	id = Column(Integer, primary_key=True, nullable=True)
+	movieimdbid = Column(String(300), unique=True)
+	movie_title = Column(String(300), nullable=False)
+	movie_rating = Column(Float, nullable=False)
+	movie_releaseyear = Column(Integer)
+	movie_votes = Column(Integer)
+
 class User(Base):
 	__tablename__ = 'roleplayer'
 
-	id = Column(Integer, primary_key=True, autoincrement=True)
-	name = Column(String(250), unique=True, nullable=True)
-	movie = relationship('Movie', backref='roleplayer')
+	id = Column(Integer, primary_key=True, nullable=True)
+	role_id = Column(String(250), unique=True)
+	name = Column(String(250), nullable=True)
+	birthyear = Column(Integer, nullable=True)
+	deathyear = Column(Integer, nullable=True)
+	primaryprofession = Column(String, nullable=True)
+	movies_acted = Column(String)
 
+	# id = Column(Integer, primary_key=True, autoincrement=True)
+	# moviename = Column(String(250), nullable=False)
+	# movie_releaseyear = Column(Integer)
+	# movie_imdb = Column(String(500), nullable=False)
+	# roleid = Column(Integer, ForeignKey('roleplayer.id'))
 
-class Movie(Base):
-	__tablename__ = 'movie'
+Base.metadata.create_all(engine)
 
-	id = Column(Integer, primary_key=True, autoincrement=True)
-	moviename = Column(String(250), nullable=False)
-	movie_releaseyear = Column(Integer)
-	movie_imdb = Column(String(500), nullable=False)
-	roleid = Column(Integer, ForeignKey('roleplayer.id'))
+SQL_Movie = """ 
+        COPY movie1(movieimdbid, movie_title, movie_rating, movie_releaseyear,movie_votes) FROM STDIN WITH
+        DELIMITER '\t' 
+			"""
 
+SQL_Role = """
+		COPY roleplayer(role_id, name, birthyear, deathyear, primaryprofession,movies_acted) FROM STDIN WITH
+        DELIMITER '\t'
+           """
+db_conn = session.connection().connection.cursor()
 
-def __init__database():
-	# create all the database tables
-	Base.metadata.create_all(engine)
+f_movie = open('data_model/movie.tsv')
+f_role = open('data_model/name.basics.tsv')
 
+db_conn = psycopg2.connect(database='movie_db', user='saranya')
 
-# # engine = create_engine('postgresql://saranya:mynewpassword@localhost/movie_db')
-# # bind the postgres to the session 
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-# Base = declarative_base()
-
-# class User(Base):
-# 	__tablename__ = 'movie_table'
-
-# 	id = Column(Integer, primary_key=True)
-# 	name = Column(String(250), unique=True, nullable=False)
-# 	moviename = Column(String(250), nullable=False)
-
-
+def generate_table_contents(SQL_Movie, SQL_Role,f_movie, f_role):
+	cursor = db_conn.cursor()
+	try:
+		cursor.copy_expert(sql=SQL_Movie,file=f_movie)
+		cursor.execute("commit;")
+		cursor.copy_expert(sql=SQL_Role, file=f_role)
+		cursor.execute("commit;")
+		cursor.close()
+	except:
+		cursor.close()
+# cursor.copy_expert("COPY Movie(movieimdbid, movie_title, movie_rating, movie_releaseyear,movie_votes) FROM '/Users/sara/Downloads/test_Movie_review/Flask_Postgres_Docker_app/data_model/movie.tsv' WITH DELIMITER '' CSV HEADER")
+# cursor.copy_expert("COPY User(role_id, name, birthyear, deathyear, primaryprofession,movies_acted) FROM '/Users/sara/Downloads/test_Movie_review/Flask_Postgres_Docker_app/data_model/name.basics.tsv'")
 # def __init__database():
 # 	# create all the database tables
 # 	Base.metadata.create_all(engine)
+try:
+	generate_table_contents(SQL_Movie, SQL_Role, f_movie, f_role)
+finally:
+	db_conn.close()
+
